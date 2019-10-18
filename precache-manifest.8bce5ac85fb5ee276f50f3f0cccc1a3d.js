bf9ead6eb6eae4d8c6fa3b3e056919097831ccaf
Binayak <csebinayak.ku@gmail.com>
2019-10-18 22:49:30 +0600

    Updates

---
+++ precache-manifest.8bce5ac85fb5ee276f50f3f0cccc1a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b14c5b64c2ab634e557a022bfa681e",
+    "url": "/github.com/law/index.html"
+  },
+  {
+    "revision": "061e30399692d73f3d6c",
+    "url": "/github.com/law/static/css/2.14b390df.chunk.css"
+  },
+  {
+    "revision": "41b6341aa4ac9d1126e1",
+    "url": "/github.com/law/static/css/main.963b8c19.chunk.css"
+  },
+  {
+    "revision": "061e30399692d73f3d6c",
+    "url": "/github.com/law/static/js/2.81b02c76.chunk.js"
+  },
+  {
+    "revision": "41b6341aa4ac9d1126e1",
+    "url": "/github.com/law/static/js/main.e63164fc.chunk.js"
+  },
+  {
+    "revision": "5cb80da029be34143743",
+    "url": "/github.com/law/static/js/runtime-main.cff7165b.js"
+  }
+]);
