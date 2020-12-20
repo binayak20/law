1caa30b7f9edd0b70a114be9bcb710ce3c2db051
binayak20 <binayak@strativ.se>
2020-12-20 10:18:49 +0600

    anchore issue fix

--- src/components/evidance/EvidenceList/EvidenceList.js
+++ src/components/evidance/EvidenceList/EvidenceList.js
@@ -1,209 +1,220 @@
-import React, { Component } from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
 import {
-  Card,
-  Table,
-  Divider,
-  Affix,
-  Button,
-  Icon,
-  Popconfirm,
-  message
-} from "antd";
-import ViewEvidence from "./ViewEvidence";
-import EvidenceModal from "../NewEvidence/EvidenceModal";
+	Card,
+	Table,
+	Divider,
+	Affix,
+	Button,
+	Icon,
+	Popconfirm,
+	message,
+} from 'antd';
+import ViewEvidence from './ViewEvidence';
+import EvidenceModal from '../NewEvidence/EvidenceModal';
 //import NoteModal from "../NewNote/NoteModal";
 function confirm(e) {
-  console.log(e);
-  message.success("Delete Successfully");
+	console.log(e);
+	message.success('Delete Successfully');
 }
 
 function cancel(e) {
-  console.log(e);
-  message.error("Click on No");
+	console.log(e);
+	message.error('Click on No');
 }
 
 const data = [
-  {
-    key: "1",
-    type: "video",
-    client: "Mithum Ray",
-    dateTime: "10/3/2019 12:29 AM",
-    evidence: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
-    name: "Mr. Jhon Murdered evidence",
-    description: "Loren spum description of jhon murdered case",
-    case: "Mr. Jhon Murdered case"
-  },
-  {
-    key: "2",
-    type: "image",
-    client: "Binayak Ray",
-    dateTime: "17/3/2019 12:29 PM",
+	{
+		key: '1',
+		type: 'video',
+		client: 'Mithum Ray',
+		dateTime: '10/3/2019 12:29 AM',
+		evidence: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+		name: 'Mr. Jhon Murdered evidence',
+		description: 'Loren spum description of jhon murdered case',
+		case: 'Mr. Jhon Murdered case',
+	},
+	{
+		key: '2',
+		type: 'image',
+		client: 'Binayak Ray',
+		dateTime: '17/3/2019 12:29 PM',
 
-    evidence: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
-    name: "Mr. Jhon Murdered evidence",
-    description: "Loren spum description of jhon murdered case",
-    case: "Mr. Jhon Murdered case"
-  },
-  {
-    key: "3",
-    type: "audio",
-    evidence: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
-    name: "Mr. Jhon Murdered evidence",
-    description: "Loren spum description of jhon murdered case",
-    case: "Mr. Jhon Murdered case",
-    client: "Molay Ray",
-    dateTime: "17/3/2019 12:29 PM"
-  },
-  {
-    key: "4",
-    type: "others",
-    evidence: "https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
-    name: "Mr. Jhon Murdered evidence",
-    description: "Loren spum description of jhon murdered case",
-    case: "Mr. Jhon Murdered case",
-    client: "Ripan Ray",
-    dateTime: "17/3/2019 12:29 PM"
-  }
+		evidence: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+		name: 'Mr. Jhon Murdered evidence',
+		description: 'Loren spum description of jhon murdered case',
+		case: 'Mr. Jhon Murdered case',
+	},
+	{
+		key: '3',
+		type: 'audio',
+		evidence: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+		name: 'Mr. Jhon Murdered evidence',
+		description: 'Loren spum description of jhon murdered case',
+		case: 'Mr. Jhon Murdered case',
+		client: 'Molay Ray',
+		dateTime: '17/3/2019 12:29 PM',
+	},
+	{
+		key: '4',
+		type: 'others',
+		evidence: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4',
+		name: 'Mr. Jhon Murdered evidence',
+		description: 'Loren spum description of jhon murdered case',
+		case: 'Mr. Jhon Murdered case',
+		client: 'Ripan Ray',
+		dateTime: '17/3/2019 12:29 PM',
+	},
 ];
 class EvidenceList extends Component {
-  state = {
-    visible: false,
-    visibleEvidence: false
-  };
-  addEvidence = () => {
-    this.setState({ visible: true });
-  };
-  updateState(name, value) {
-    this.setState({ [name]: value });
-  }
-  viewDetails(record) {
-    console.log(record);
-    this.setState({
-      evidenceData: record,
-      visible: true
-    });
-  }
-  viewEvidence(record) {
-    console.log(record);
-    this.setState({
-      evidence: record.evidence,
-      visibleEvidence: true,
-      evidenceType:record.type
-    });
-  }
-  render() {
-    const { visible, evidenceData, visibleEvidence, evidence, evidenceType } = this.state;
-    const columns = [
-      {
-        title: "Type",
-        dataIndex: "type",
-        key: "type",
-        render: (text, record) => (
-          <a className="href-tag" onClick={() => this.viewDetails(record)}>
-            {text}
-          </a>
-        )
-      },
-      {
-        title: "Evidence",
-        dataIndex: "evidence",
-        key: "evidence",
-        render: (text, record) => (
-          <Icon
-            onClick={() => this.viewEvidence(record)}
-            style={{ color: " #a98067", fontSize: 30, cursor: "pointer" }}
-            type={
-              record.type === "video"
-                ? "video-camera"
-                : record.type === "audio"
-                ? "audio"
-                : record.type === "image"
-                ? "file-image"
-                : "file"
-            }
-          />
-        )
-      },
-      {
-        title: "Name",
-        dataIndex: "name",
-        key: "name"
-      },
-      //   {
-      //     title: "Description",
-      //     dataIndex: "description",
-      //     key: "description"
-      //   },
-      {
-        title: "Date & Time",
-        dataIndex: "dateTime",
-        key: "dateTime"
-      },
-      {
-        title: "Client",
-        dataIndex: "client",
-        key: "client"
-      },
-      {
-        title: "Case",
-        dataIndex: "case",
-        key: "case"
-      },
+	state = {
+		visible: false,
+		visibleEvidence: false,
+	};
+	addEvidence = () => {
+		this.setState({ visible: true });
+	};
+	updateState(name, value) {
+		this.setState({ [name]: value });
+	}
+	viewDetails(record) {
+		console.log(record);
+		this.setState({
+			evidenceData: record,
+			visible: true,
+		});
+	}
+	viewEvidence(record) {
+		console.log(record);
+		this.setState({
+			evidence: record.evidence,
+			visibleEvidence: true,
+			evidenceType: record.type,
+		});
+	}
+	render() {
+		const {
+			visible,
+			evidenceData,
+			visibleEvidence,
+			evidence,
+			evidenceType,
+		} = this.state;
+		const columns = [
+			{
+				title: 'Type',
+				dataIndex: 'type',
+				key: 'type',
+				render: (text, record) => (
+					<a
+						href='#'
+						className='href-tag'
+						onClick={() => this.viewDetails(record)}
+					>
+						{text}
+					</a>
+				),
+			},
+			{
+				title: 'Evidence',
+				dataIndex: 'evidence',
+				key: 'evidence',
+				render: (text, record) => (
+					<Icon
+						onClick={() => this.viewEvidence(record)}
+						style={{ color: ' #a98067', fontSize: 30, cursor: 'pointer' }}
+						type={
+							record.type === 'video'
+								? 'video-camera'
+								: record.type === 'audio'
+								? 'audio'
+								: record.type === 'image'
+								? 'file-image'
+								: 'file'
+						}
+					/>
+				),
+			},
+			{
+				title: 'Name',
+				dataIndex: 'name',
+				key: 'name',
+			},
+			//   {
+			//     title: "Description",
+			//     dataIndex: "description",
+			//     key: "description"
+			//   },
+			{
+				title: 'Date & Time',
+				dataIndex: 'dateTime',
+				key: 'dateTime',
+			},
+			{
+				title: 'Client',
+				dataIndex: 'client',
+				key: 'client',
+			},
+			{
+				title: 'Case',
+				dataIndex: 'case',
+				key: 'case',
+			},
 
-      {
-        title: "Action",
-        key: "action",
-        render: (text, record) => (
-          <span>
-            <Icon
-              type="edit"
-              style={{ color: "green", fontWeight: "bold", cursor: "pointer" }}
-            />
-            <Divider type="vertical" />
+			{
+				title: 'Action',
+				key: 'action',
+				render: (text, record) => (
+					<span>
+						<Icon
+							type='edit'
+							style={{ color: 'green', fontWeight: 'bold', cursor: 'pointer' }}
+						/>
+						<Divider type='vertical' />
 
-            <Popconfirm
-              title="Are you sure delete this task?"
-              onConfirm={confirm}
-              onCancel={cancel}
-              okText="Yes"
-              cancelText="No"
-            >
-              <Icon
-                type="close-square"
-                style={{ color: "red", fontWeight: "bold", cursor: "pointer" }}
-              />
-            </Popconfirm>
-          </span>
-        )
-      }
-    ];
-    return (
-      <Card>
-        <EvidenceModal
-          updateState={this.updateState.bind(this)}
-          visible={visible}
-          evidenceDetails={evidenceData}
-        />
-        <ViewEvidence
-          updateState={this.updateState.bind(this)}
-          visibleEvidence={visibleEvidence}
-          evidence={evidence}
-          evidenceType={evidenceType}
-        />
-        <Affix className="FAB">
-          <Button
-            className="affix-button"
-            shape="circle"
-            size="large"
-            onClick={this.addEvidence}
-            icon="plus"
-          />
-        </Affix>
-        <h2>Evidence List</h2>
-        <Table columns={columns} dataSource={data} />
-      </Card>
-    );
-  }
+						<Popconfirm
+							title='Are you sure delete this task?'
+							onConfirm={confirm}
+							onCancel={cancel}
+							okText='Yes'
+							cancelText='No'
+						>
+							<Icon
+								type='close-square'
+								style={{ color: 'red', fontWeight: 'bold', cursor: 'pointer' }}
+							/>
+						</Popconfirm>
+					</span>
+				),
+			},
+		];
+		return (
+			<Card>
+				<EvidenceModal
+					updateState={this.updateState.bind(this)}
+					visible={visible}
+					evidenceDetails={evidenceData}
+				/>
+				<ViewEvidence
+					updateState={this.updateState.bind(this)}
+					visibleEvidence={visibleEvidence}
+					evidence={evidence}
+					evidenceType={evidenceType}
+				/>
+				<Affix className='FAB'>
+					<Button
+						className='affix-button'
+						shape='circle'
+						size='large'
+						onClick={this.addEvidence}
+						icon='plus'
+					/>
+				</Affix>
+				<h2>Evidence List</h2>
+				<Table columns={columns} dataSource={data} />
+			</Card>
+		);
+	}
 }
 
 export default EvidenceList;
--- src/components/lawBook/BookList.js
+++ src/components/lawBook/BookList.js
@@ -1,81 +1,89 @@
-import React, { Component } from "react";
-import { Row, Col, Card, Icon, Avatar,List, Typography } from "antd";
-import { Link } from "react-router-dom";
-import { category, bookData } from "../../const";
+import React, { Component } from 'react';
+import { Row, Col, Card, List, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import { category, bookData } from '../../const';
 const { Meta } = Card;
 
 class BookList extends Component {
-  state={
-    bookList:[]
-  }
-  componentDidMount(){
-    this.setState({bookList:bookData[0].book, markId:bookData[0].catId})
-  }
-  getCategory() {
-    const listItems = category.map(cat => <li key={cat}><a>{cat}</a></li>);
-    return listItems;
-  }
-  clickCategory=(item)=>{
-    let list = bookData.find((element)=>{
-      if(element.catId === item){
-        return element.book
-    }});
-    this.setState({bookList:list.book,  markId:list.catId})
-  //  console.log(bookList)
-  }
-  render() {
-    const {bookList,markId}= this.state;
-    console.log(bookList)
-    return (
-      <div>
-        <Row>
-          <Col span={6}>
-            {/* {this.getCategory()} */}
-            {/* <h3 style={{ marginBottom: 16 }}>Default Size</h3> */}
-              <List
-                header={<h3>Type of Laws</h3>}
-                // footer={<div>Footer</div>}
-                bordered={false}
-                dataSource={category}
-                renderItem={(item, index) => (
-                  <List.Item>
-                    <Typography.Text mark={index===markId}>{index+1}. </Typography.Text><a onClick={()=>this.clickCategory(index)}> {item}</a>
-                  </List.Item>
-                )}
-              />
-          </Col>
-          <Col span={18}>
-            <Row gutter={16}>
-              {bookList.map((key, index) => (
-                <Col span={6} key={index}>
-                  <Link to="/chapter-section">
-                    <Card
-                      style={{ width: 180, marginBottom: 20 }}
-                      hoverable
-                      className="book-list-card"
-                      cover={
-                        <img
-                          alt="example"
-                          src="Stadiyum Assets/Assets/AL.jpg"
-                          height={150}
-                          width={180}
-                        />
-                      }
-                    >
-                      <Meta
-                        title="The Book Of the law"
-                        description="This is the description"
-                      />
-                    </Card>
-                  </Link>
-                </Col>
-              ))}
-            </Row>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+	state = {
+		bookList: [],
+	};
+	componentDidMount() {
+		this.setState({ bookList: bookData[0].book, markId: bookData[0].catId });
+	}
+	getCategory() {
+		const listItems = category.map((cat) => (
+			<li key={cat}>
+				<a>{cat}</a>
+			</li>
+		));
+		return listItems;
+	}
+	clickCategory = (item) => {
+		let list = bookData.find((element) => {
+			if (element.catId === item) {
+				return element.book;
+			}
+		});
+		this.setState({ bookList: list.book, markId: list.catId });
+		//  console.log(bookList)
+	};
+	render() {
+		const { bookList, markId } = this.state;
+		console.log(bookList);
+		return (
+			<div>
+				<Row>
+					<Col span={6}>
+						{/* {this.getCategory()} */}
+						{/* <h3 style={{ marginBottom: 16 }}>Default Size</h3> */}
+						<List
+							header={<h3>Type of Laws</h3>}
+							// footer={<div>Footer</div>}
+							bordered={false}
+							dataSource={category}
+							renderItem={(item, index) => (
+								<List.Item>
+									<Typography.Text mark={index === markId}>
+										{index + 1}.{' '}
+									</Typography.Text>
+									<a onClick={() => this.clickCategory(index)}> {item}</a>
+								</List.Item>
+							)}
+						/>
+					</Col>
+					<Col span={18}>
+						<Row gutter={16}>
+							{bookList.map((key, index) => (
+								<Col span={6} key={index}>
+									<Link to='/chapter-section'>
+										<Card
+											style={{ width: 180, marginBottom: 20 }}
+											hoverable
+											className='book-list-card'
+											cover={
+												<img
+													alt='example'
+													src='Stadiyum Assets/Assets/AL.jpg'
+													height={150}
+													width={180}
+												/>
+											}
+										>
+											<Meta
+												title='The Book Of the law'
+												description='This is the description'
+											/>
+										</Card>
+									</Link>
+								</Col>
+							))}
+						</Row>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
 export default BookList;
